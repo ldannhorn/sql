2128a3a4aff66855f8c050298fd85801ca8ebf0b
Lion Dannhorn <lion.dannhorn@gymnasium-oedeme.de>
2025-02-12 11:20:50 +0100

    Klasse Interpreter

---
+++ Interpreter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSQL
+{
+    class Interpreter
+    {
+        
+        private Table[] tables;
+
+        public Interpreter(Table[] tables)
+        {
+            this.tables = tables;
+        }
+
+        public string Interpret(string query)
+        {
+
+            string[] args = query.Split(' ');
+
+            switch (args[0].ToLower())
+            {
+                case "insert":
+                    Insert(args);
+                    break;
+            }
+
+
+            return "";
+        }
+
+        private bool Insert(string[] query)
+        {
+            return false;
+        }
+
+
+    }
+}
